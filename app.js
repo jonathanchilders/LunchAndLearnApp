8e69534989c0f9f6f2daec302ba5f031c7c5328d
Jonathan Childers <jonathanchilders65@gmail.com>
2016-09-19 07:55:51 -0700

    Added First screen

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("Hello world");
+//Require dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars');
+
+//Paths to routes
+var index = require('./routes/index');
+
+var app = express();
+
+//All environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// Add routes here
+app.get('/', index.view);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
